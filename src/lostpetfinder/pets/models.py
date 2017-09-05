14744878908229e2fd1d46957da7ba7403fd7bfb
Tony <gus3656@gmail.com>
2017-09-05 17:36:25 +1000

    Fixed UI

--- src/lostpetfinder/pets/models.py
+++ src/lostpetfinder/pets/models.py
@@ -109,3 +109,18 @@ def rl_pre_save_receiver(sender, instance, *args, **kwargs):
 
 pre_save.connect(rl_pre_save_receiver, sender=Pet)
 #post_save.connect(rl_post_save_receiver, sender=Pet)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Pet)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
