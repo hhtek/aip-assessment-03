0b3814ae18080000a8d575084bf5b4090da2883b
Henry Ho <henry.hho@outlook.com>
2017-09-27 08:47:16 +1000

    Update .gitignore and base.py

--- src/lostpetfinder/lostpetfinder/settings/base.py
+++ src/lostpetfinder/lostpetfinder/settings/base.py
@@ -29,8 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'lostpetfinderau@gmail.com'
-EMAIL_HOST_PASSWORD = 'LpfAiputs17'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Lost Pet Finder <no_reply@lostpetfinder.com.au'
